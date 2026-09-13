a12d9cd8d2fe7d41bc89a108e1682d8c42e0f8dd
malaya ranjan pradhani <malayaranjan079@gmail.com>
2026-09-14 00:06:57 +0530

    creating doctor dashboard

---
+++ doctor/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path 
+
+from doctor import views
+
+app_name="doctor"
+
+
+urlpatterns = [
+    path("",views.dashboard,name="dashboard"),
+    # path("appointments/",views.appointments,name="appointments"),
+
+]
