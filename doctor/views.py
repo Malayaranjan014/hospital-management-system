e5c7b5c7e084ac6880263e2c3342958e02bb7874
malaya ranjan pradhani <malayaranjan079@gmail.com>
2026-09-14 14:25:00 +0530

    add doctor appointment like  shedule appointment and cancle and complete appointment

--- doctor/views.py
+++ doctor/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from doctor import models as doctor_models
 from base import models as base_models
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -53,3 +55,42 @@ def appointment_detail(request, appointment_id):
     }
 
     return render(request, "doctor/appointment_detail.html", context)
+
+
+
+@login_required
+def cancel_appointment(request, appointment_id):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointment = base_models.Appointment.objects.get(appointment_id=appointment_id, doctor=doctor)
+
+    appointment.status = "Cancelled"
+    appointment.save()
+
+    messages.success(request, "Appointment Cancelled Successfully")
+    return redirect("doctor:appointment_detail", appointment.appointment_id)
+
+
+
+@login_required
+def activate_appointment(request, appointment_id):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointment = base_models.Appointment.objects.get(appointment_id=appointment_id, doctor=doctor)
+
+    appointment.status = "Scheduled"
+    appointment.save()
+
+    messages.success(request, "Appointment Re-Scheduled Successfully")
+    return redirect("doctor:appointment_detail", appointment.appointment_id)
+
+
+
+@login_required
+def complete_appointment(request, appointment_id):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointment = base_models.Appointment.objects.get(appointment_id=appointment_id, doctor=doctor)
+
+    appointment.status = "Completed"
+    appointment.save()
+
+    messages.success(request, "Appointment Completed Successfully")
+    return redirect("doctor:appointment_detail", appointment.appointment_id)
