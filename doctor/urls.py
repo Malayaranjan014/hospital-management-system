b26faaeaf33df9c092d2549b137189367297e1e9
malaya ranjan pradhani <malayaranjan079@gmail.com>
2026-09-14 20:42:16 +0530

    Add lab test creation and update functionality

--- doctor/urls.py
+++ doctor/urls.py
@@ -10,10 +10,17 @@ urlpatterns = [
     path("appointments/",views.appointments,name="appointments"),
     path("appointments/<appointment_id>",views.appointment_detail,name="appointment_detail"),
     path("cancel_appointment/<appointment_id>",views.cancel_appointment,name="cancel_appointment"),
+
     path("activate_appointment/<appointment_id>",views.activate_appointment,name="activate_appointment"),
     path("complete_appointment/<appointment_id>",views.complete_appointment,name="complete_appointment"),
+
     path("add_medical_report/<appointment_id>",views.add_medical_report,name="add_medical_report"),
     path("edit_medical_report/<appointment_id>/<medical_report_id>/",views.edit_medical_report ,name="edit_medical_report"),
+
+
+    path("add_lab_test/<appointment_id>",views.add_lab_test,name="add_lab_test"),
+    path("edit_lab_test/<appointment_id>/<lab_test_id>/",views.edit_lab_test ,name="edit_lab_test"),
+    
 
 
 ]
